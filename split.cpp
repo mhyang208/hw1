0dc98d7ccffef8509ccecbc1e4f0b6d9147e2cec
Madison Yang <mhyang@usc.edu>
2025-01-30 01:29:24 -0800

    Finished writing split

--- split.cpp
+++ split.cpp
@@ -18,6 +18,54 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+
+  // remember recursion means calling the function again with a smaller section of the list
+  // need to delete the pointer of the Node we've already processed
+  // PROBLEM: if we just push_front to odds and evens, these lists will be reverse sorted
+    // could push to end but would need to iterate to end of list every time...
+    // can't use a loop either so maybe not
+    // well maybe can recursively find end of list...
+    // or reverse the two new lists afterward
+
+  // check if in is null to avoid segfaults
+  if (in != nullptr) {
+    // sort the given Node
+    if (in -> value % 2 == 0) {
+        if (evens == nullptr) {
+          evens = new Node{in->value, nullptr};
+        }
+        else {
+          evens = new Node{in->value, evens};
+        }
+    }
+    else {
+      if (odds == nullptr) {
+          odds = new Node{in->value, nullptr};
+        }
+        else {
+          odds = new Node{in->value, odds};
+        }
+    }
+
+    // skips this if it's the base case (end of list)
+    if (in -> next != nullptr) {
+      // delete the processed Node
+      // NOTE: in now points to the Node that originally came after it
+      Node* temp = in;
+      in = in -> next;
+      delete temp;
+      
+      // recursive call
+      split(in, odds, evens);
+    }
+    else {
+      // we've reached the end of this list, simply delete the processed Node
+      delete in;
+      in = nullptr;
+    }
+  }
+
+
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -9,9 +9,66 @@ You can compile this file like this:
 g++ split.cpp test_split.cpp -o test_split
 */
 
+#include <iostream>
+
 #include "split.h"
+
+void print_list(Node* list) {
+
+    if (list == nullptr) {
+        std::cout << "nullptr" << std::endl;
+        return;
+    }
+
+    for (Node* p = list; p != nullptr; p = p->next) {
+        std::cout << p -> value << " ";
+        std::cout << std::endl;
+    }
+}
+
+void delete_list(Node* list) {
+    if (list != nullptr) {
+        for (Node* p  = list; p != nullptr;) {
+            Node* temp = p;
+            p = p->next;
+            delete p;
+        }
+    }
+}
 
 int main(int argc, char* argv[])
 {
+    Node* test_evens = nullptr;
+    Node* test_odds = nullptr;
+    
+    Node* test_in = nullptr;
+
+    // test if all three are null inputs
+    split(test_in, test_evens, test_odds);
+
+    // test if split deletes test_in correctly
+    test_in = new Node(2, nullptr);
+    split(test_in, test_evens, test_odds);
+
+    print_list(test_in);
+    print_list(test_evens);
+
+    delete_list(test_evens);
+    
+
+    // test normal case
+    test_in = new Node(9, nullptr);
+    test_in = new Node(8, test_in);
+    test_in = new Node(5, test_in);
+    test_in = new Node(2, test_in);
+
+    split(test_in, test_evens, test_odds);
+
+    print_list(test_in);
+    print_list(test_evens);
+    print_list(test_odds);
+
+    delete_list(test_evens);
+    delete_list(test_odds);
 
 }
