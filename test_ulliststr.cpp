d45d050f27fdd9641650aa5ebaf90ae24fc4edf2
Madison Yang <mhyang@usc.edu>
2025-01-31 01:32:40 -0800

    Finished ulliststr implementation

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -7,7 +7,85 @@
 
 //Use this file to test your ulliststr implementation before running the test suite
 
+
 int main(int argc, char* argv[])
 {
+
+    // test pushing to an empty list
+    ULListStr test_ulliststr1;
+    test_ulliststr1.push_front("0");
+
+    // std::cout << "test";
+    std::cout << test_ulliststr1.front() << std::endl;
+    std::cout << test_ulliststr1.back() << std::endl;
+    std::cout << test_ulliststr1.size() << std::endl;
+
+    test_ulliststr1.clear();
+
+    // test pushing to a list with space
+    ULListStr test_ulliststr2;
+    for (int i = 0; i < 5; i++)
+        test_ulliststr2.push_front("0");
+
+    std::cout << test_ulliststr2.size() << std::endl;
+    // testing size of list
+    std::cout << "get" << std::endl;
+    std::cout << test_ulliststr2.get(4) << std::endl;
+    // std::cout << test_ulliststr2.back() << std::endl;
+
+    test_ulliststr2.clear();
+
+    for (int i = 0; i < 5; i++)
+        test_ulliststr2.push_back("0");
+
+
+    std::cout << "Back" << std::endl;
+    std::cout << test_ulliststr2.size() << std::endl;
+    // testing size of list
+    std::cout << "get" << std::endl;
+    std::cout << test_ulliststr2.get(4) << std::endl;
+
+    // test pushing to a list without space
+    ULListStr test_ulliststr3;
+    for (int i = 0; i < 10; i++)
+        test_ulliststr3.push_front("0");
+
+    std::cout << "get" << std::endl;
+    std::cout << test_ulliststr3.get(9) << std::endl;
+    std::cout << test_ulliststr3.size() << std::endl;
+
+    test_ulliststr3.push_front("1");
+    std::cout << "get" << std::endl;
+    std::cout << test_ulliststr3.get(10) << std::endl;
+    std::cout << test_ulliststr3.size() << std::endl;
+
+    test_ulliststr3.clear();
+    
+
+    for (int i = 0; i < 10; i++)
+        test_ulliststr3.push_back("0");
+
+    std::cout << "Back" << std::endl;
+    std::cout << "get" << std::endl;
+    std::cout << test_ulliststr3.get(9) << std::endl;
+    std::cout << test_ulliststr3.size() << std::endl;
+
+    test_ulliststr3.push_back("1");
+    std::cout << "get" << std::endl;
+    std::cout << test_ulliststr3.get(10) << std::endl;
+    std::cout << test_ulliststr3.size() << std::endl;
+
+    // test front then back
+    ULListStr test_ulliststr4;
+    test_ulliststr4.push_front("0");
+    test_ulliststr4.push_back("1");
+
+    std::cout << std::endl;
+    std::cout << "FrontBack" << std::endl;
+    std::cout << "get" << std::endl;
+    std::cout << test_ulliststr4.get(0) << std::endl;
+    std::cout << "get" << std::endl;
+    std::cout << test_ulliststr4.get(1) << std::endl;
+
 
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -2,6 +2,8 @@
 #include <stdexcept>
 #include "ulliststr.h"
 
+#include <iostream>
+
 ULListStr::ULListStr()
 {
   head_ = NULL;
@@ -25,6 +27,238 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+
+std::string* ULListStr::getValAtLoc(size_t loc) const {
+
+  // std::cout << "called" << std::endl;
+  // std::cout << loc << std::endl;
+
+
+  // check if loc is within size
+  if (loc >= size_) {
+    // std::cout << "bad loc" << std::endl;
+    return nullptr;
+  }
+  else {
+
+    // std::cout << "else" << std::endl;
+
+    // every Item has an array of 10
+    Item* p = head_;
+    // std::cout << "next_check" << std::endl;
+    // std::cout << (p->next == nullptr) << std::endl;
+    // std::cout << p->first << std::endl;
+    // std::cout << ((loc + p->first) / 10) << std::endl;
+  
+    // placeholder location to edit as we progress
+    size_t location;
+
+    // move along Items until we reach the one the location is in
+    for (size_t i = 0; i < ((loc + head_->first) / 10); i++) {
+      p = p->next;
+    }
+
+    // find "index" of element we want within that Item (accounting for the shift from head_->first)
+    location = (loc + head_->first) % 10;
+
+    std::cout << "Head: " << (head_->next == nullptr) << std::endl;
+
+    std::cout << "Loc: " << loc << std::endl;
+    std::cout << "First: " << head_->first << std::endl;
+    std::cout << "Location: " << location << std::endl;
+
+    // return that element
+    std::cout << "Value: " << p->val[location] << std::endl;
+    std::cout << "Return" << std::endl;
+    return &(p->val[location]);
+
+
+
+    // go to the correct Item
+    // NOTE:: need to account for if the very first node of the overall list doesn't start from the top
+    // for (size_t i = 0; i < (loc / 10); i++) {
+    //   // std::cout << (loc + p->first) / 10 << std::endl;
+    //   // std::cout << "loop" << std::endl;
+    //   p = p->next;
+    // }
+
+    // location = loc % 10;
+
+    // // last check since loop doesn't cover all cases
+    // if (loc % 10 > (10 - p->first)) {
+    //   p = p->next;
+    //   location -= p->first;
+    // }
+
+    // // we're now at the start of the Item that we want to extract from
+    // // return the correct value in that Item
+    // // std::cout << "return" << std::endl;
+    // // std::cout << location << std::endl;
+    // return &(p->val[location]);
+
+    // static std::string* result = &(p->val[(10 - p->first) + (loc % 10)]);
+    // return &(p->val[p->first + (loc % (10 - p->first))]);
+  }
+}
+
+std::string const& ULListStr::back() const {
+  return tail_->val[tail_->last-1];
+}
+
+std::string const& ULListStr::front() const {
+  return head_->val[head_->first];
+}
+
+void ULListStr::push_front(const std::string& val) {
+  // check if list is empty
+  if (head_ == NULL) {
+    // create a new item
+    head_ = new Item();
+    head_->first = 9;
+    head_->last = 10;
+    head_->prev = nullptr;
+    head_->next = nullptr;
+    tail_ = head_;
+
+    // add value to the array in the new Item
+    head_ ->val[9] = val;
+  }
+
+  // check if there's space in the first Item, if not then make a new Item
+  else if (head_->first == 0) {
+    // std::cout << "new" << std::endl;
+    Item* temp = head_;
+    head_ = new Item();
+    head_->first = 9;
+    head_->last = 10;
+    head_->prev = nullptr;
+    head_->next = temp;
+    
+
+    // change head pointer
+    temp->prev = head_;
+
+    // std::cout << "next check" << std::endl;
+    // std::cout << (head_->next == nullptr) << std::endl;
+
+    // add value to the array in the new Item
+    head_->val[9] = val;
+  }
+
+  // otherwise push the value to the front of head array
+  else {
+    head_->first -= 1;
+    head_->val[head_->first] = val;
+  }
+
+  // update size
+  size_ += 1;
+
+}
+
+void ULListStr::push_back(const std::string& val) {
+  // check if list is empty
+  if (tail_ == NULL) {
+    // create a new item
+    tail_ = new Item();
+    tail_->first = 0;
+    tail_->last = 1;
+    tail_->prev = nullptr;
+    tail_->next = nullptr;
+    head_ = tail_;
+
+    // add value to the array in the new Item
+    tail_->val[0] = val;
+  }
+  // check if there's space in the last Item, if not then make a new Item
+  else if (tail_->last == 10) {
+    // std::cout << "new" << std::endl;
+    Item* temp = tail_;
+    tail_ = new Item();
+    tail_->first = 0;
+    tail_->last = 1;
+    tail_->prev = temp;
+    tail_->next = nullptr;
+
+    // change tail pointer
+    temp->next = tail_;
+
+    // std::cout << "next check" << std::endl;
+    // std::cout << (head_->next == nullptr) << std::endl;
+
+    // add value to the array in the new Item
+    tail_->val[0] = val;
+  }
+
+  // otherwise push the value to the front of head array
+  else {
+    tail_->last += 1;
+    tail_->val[tail_->last-1] = val;
+  }
+  // update size
+  size_ += 1;
+
+}
+
+void ULListStr::pop_front() {
+  // check if list is empty
+  if (head_ != NULL) {
+    // check if element to pop is only one in that Item
+    if (head_->first == 9) {
+      // check if that Item is the only one in the list
+      if (head_ == tail_) {
+        // delete the Item and set head and tail to NULL
+        tail_ = NULL;
+        delete head_;
+        head_ = NULL;
+      }
+      else {
+        // delete the Item when we pop
+        Item* temp = head_;
+        head_ = head_->next;
+        head_->prev = nullptr;
+        delete temp;
+      }
+    }
+    else {
+      // "remove" the last element in that Item
+      head_->first += 1;
+    }
+
+    // edit size
+    size_ -= 1;
+  }
+}
+
+void ULListStr::pop_back() {
+  // check if list is empty
+  if (head_ != NULL) {
+    // check if element to pop is only one in that Item
+    if (tail_->last == 1) {
+      // check if that Item is the only one in the list
+      if (head_ == tail_) {
+        // delete the Item and set head and tail to NULL
+        tail_ = NULL;
+        delete head_;
+        head_ = NULL;
+      }
+      else {
+        // delete the Item when we pop
+        Item* temp = tail_;
+        tail_ = tail_->prev;
+        tail_->next = nullptr;
+        delete temp;
+      }
+    }
+    else {
+      // "remove" the first element in that Item
+      tail_->last -= 1;
+    }
+
+    // edit size
+    size_ -= 1;
+  }
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
